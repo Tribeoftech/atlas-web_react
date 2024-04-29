fe2cf0b281ccbb34a77cd760f654453911a5c596
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-29 15:27:24 -0500

    path fix

--- Webpack/task_1/webpackconfig.js
+++ Webpack/task_1/webpackconfig.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'production',
-	entry: './js/dashboard_main.js',
+	entry: './task_1/js/dashboard_main.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'public')
