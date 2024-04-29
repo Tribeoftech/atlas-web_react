b05a1811c9eec22886f4ab767446873f23a8bf20
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-29 10:57:25 -0500

    index.js

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+//jquery import thanks to babel
+
+import $ from 'jquery';
+
+//tags
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
