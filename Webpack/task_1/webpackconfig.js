ca64c13a2c5fd2b90a4e93e17aa29455ac2389b2
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-29 12:48:33 -0500

    changes

--- Webpack/task_1/webpackconfig.js
+++ Webpack/task_1/webpackconfig.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
-module.exports = {
-	mode: 'production',
-	entry: './js/dashboard_main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public')
-	}
+const productionConfig = {
+  mode: 'production',
+  entry: 'task_1/js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
 };
+
+const developmentConfig = {
+  mode: 'development',
+  // Add other webpack configuration options for development mode if needed
+};
+
+module.exports = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
