643c6056d6fa4b2a54e015eef69b0f64c4e23325
Tribeoftech <gratefulgrowing333@gmail.com>
2024-05-02 15:49:00 -0500

    fixing presviously pushed imports

--- react_intro/task_5/dashboard/config/setupTests.js
+++ react_intro/task_5/dashboard/config/setupTests.js
@@ -5,7 +5,7 @@
 // import '@testing-library/jest-dom';
 require('jsdom-global')();
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-17';
 
 // Configure Enzyme with the adapter
 Enzyme.configure({ adapter: new Adapter() });
