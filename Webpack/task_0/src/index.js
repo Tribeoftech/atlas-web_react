2d598bd850c4ad7aab215a309b0d1bf4e3aad0c7
Tribeoftech <gratefulgrowing333@gmail.com>
2024-05-11 15:37:22 -0500

    fix

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
-// jquery import thanks to babel
+// interesting jquery import thanks to babel
 import $ from 'jquery';
 
-// add tags to body
+// add three different p tags to body
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
