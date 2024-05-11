1089ad7b51ecea24f79c3bb2ba64f93cf22731c2
Tribeoftech <gratefulgrowing333@gmail.com>
2024-05-11 09:20:49 -0500

    gh-pages

--- react_intro/functional_backup/dashboard/config/setupTests.js
+++
@@ -1,5 +0,0 @@
-require('jsdom-global')();
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-// Configure Enzyme with the adapter
-Enzyme.configure({ adapter: new Adapter() });
