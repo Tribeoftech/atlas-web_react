835770c6a7a085d0a22647d6978fb90f707f1b23
Tribeoftech <gratefulgrowing333@gmail.com>
2024-05-14 09:39:00 -0500

    modified CourseListRow

--- React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.test.js
+++ React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import CourseListRow from './CourseListRow;
+import CourseListRow from './CourseListRow';
 
 
 // shallow render CourseListRow component
