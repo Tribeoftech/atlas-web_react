9fe4c9a67703920bf801fb0b3b3f7b427a4c213c
Tribeoftech <gratefulgrowing333@gmail.com>
2024-05-15 13:12:49 -0500

    fixing test

--- React_inline_styling/task_4/dashboard/config/setupTests.js
+++ React_inline_styling/task_4/dashboard/config/setupTests.js
@@ -6,7 +6,7 @@ Enzyme.configure({ adapter: new Adapter() });
 
 const shallow = Enzyme.shallow;
 const mount = Enzyme.mount;
-const unmount = Enzyme.unmount;
+const unmount = Enzyme.unmoun;
 
 
 export { shallow, mount, unmount };
