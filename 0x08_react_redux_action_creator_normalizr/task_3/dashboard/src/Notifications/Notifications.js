a35ce1ed4171454e1dfccd554ad407ce5969b9c5
Tribeoftech <gratefulgrowing333@gmail.com>
2024-05-19 17:59:22 -0500

    getallfuncbyusr

--- 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/Notifications/Notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/Notifications/Notifications.js
@@ -67,7 +67,22 @@ class Notification extends PureComponent {
 		)
 	}
 }
-
+export const getAllNotificationsByUser = (userId) => {
+	// returns a list containing all 'context' objects from the normalizedData variable
+	// when the author id matches the userId parameter
+	//
+	// @userId: string
+	//
+	// returns: list containing all 'context' objects when 
+	// the author id matches the userId parameter
+	const myList = [];
+	jsonData.forEach((notification) => {
+	  if (notification.author.id === userId) {
+		myList.push(notification.context);
+	  }
+	})
+	return myList;
+  }
 const notificationStyles = StyleSheet.create({
 	notifications: {
 		'@media (min-width: 350px)': {
--- 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/schema/notifications.js
@@ -1,24 +1,6 @@
-const jsonData = require('../notifications.json');
+const jsonData = require('../../notifications.json');
 const schema = require('normalizr').schema;
 const normalize = require('normalizr').normalize;
-
-
-export const getAllNotificationsByUser = (userId) => {
-  // returns a list containing all 'context' objects from the jsonData variable
-  // when the author id matches the userId parameter
-  //
-  // @userId: string
-  //
-  // returns: list containing all 'context' objects when 
-  // the author id matches the userId parameter
-  const myList = [];
-  jsonData.forEach((notification) => {
-    if (notification.author.id === userId) {
-      myList.push(notification.context);
-    }
-  })
-  return myList;
-}
 
 
 const user = new schema.Entity('users');
@@ -33,3 +15,20 @@ const notification = new schema.Entity('notifications', {
 });
 
 export const normalizedData = normalize(jsonData, [notification]);
+
+export const getAllNotificationsByUser = (userId) => {
+  // returns a list containing all 'context' objects from the normalizedData variable
+  // when the author id matches the userId parameter
+  //
+  // @userId: string
+  //
+  // returns: list containing all 'context' objects when 
+  // the author id matches the userId parameter
+  const myList = [];
+  jsonData.forEach((notification) => {
+    if (notification.author.id === userId) {
+      myList.push(notification.context);
+    }
+  })
+  return myList;
+}
